52d5ee99ac013133acc41b626a243dc03e053e2b
FirqetDadashov <dadashovfirqet@gmail.com>
2024-06-19 15:35:33 +0400

    Tapsiriqlarilk

---
+++ task1/Program.cs
@@ -0,0 +1,82 @@
+﻿
+
+//tapsiriq 1-2
+/*string name2 = "Firqet";
+Console.WriteLine("adnizi daxil et");
+string name = Console.ReadLine();
+Console.WriteLine($"name: {name}");
+
+if (name == name2)
+{
+    Console.WriteLine("Yes");
+}
+else {
+    Console.WriteLine("No");
+}
+*/
+
+//tapsiriq 1-1
+/*int a = 2424;
+int b = a % 7;
+int c = b % 3;
+bool df = false;
+bool fd = true;
+
+
+if (c == 0)
+{
+    Console.WriteLine(fd);
+}else if (b == 0) {  Console.WriteLine(fd); 
+}
+else {  Console.WriteLine(df); }*/
+
+
+
+
+//tapsiriq 1-3
+Console.WriteLine("ededi daxil edin:");
+string reqem = Console.ReadLine();
+
+char sonreqem = reqem[reqem.Length - 1];
+
+Console.WriteLine($"ededin sagdan ilk reqemi: {sonreqem}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Console.WriteLine("reqem daxil edin:");
+string input = Console.ReadLine();
+int a = int.Parse(input);
+Console.WriteLine("reqem: " + a);*/
+
+/*
+Console.WriteLine("ededi daxil edin:");
+string reqem = Console.ReadLine();
+
+// Doğal sayının son rakamını elde etmek için string'in son karakterini alın
+char sonreqem = reqem[reqem.Length  - 1];
+
+Console.WriteLine($"daxildir: {sonreqem}");*/
+
+
+
+
+
+
+
+
+
+
+
+
+
